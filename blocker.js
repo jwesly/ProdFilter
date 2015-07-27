0cca605e2cd35544a861a834e17ad29161395c40
Jabez Wesly <wjabezkingsly@gmail.com>
2015-07-27 19:00:50 -0400

    Blocker Working

--- blocker.js
+++ blocker.js
@@ -1,2 +1,36 @@
 //js that runs on every page and blocks some of them
-console.log("this is running..........");
+var block = function(){
+	document.head.innerHTML = '';
+	document.body.innerHTML = '<h1>Your Productivity Filter Has Blocked This Page. Get Back on Track!';
+}
+
+console.log(document.URL);
+chrome.storage.sync.get('options',function(result){
+	for (var i = 0; i < result.options.length; i++){
+		var row = result.options[i];
+		console.log(row);
+		var pattern = new RegExp(row.patternz,'i');
+		if(pattern.test(document.URL)){
+			var d = new Date();
+			var index = d.getDay();
+			console.log(row.repeat,index)
+			if((row.repeat == "Weekdays" && index > 0 && index < 5) || (row.repeat == "Weekends" && (index == 0 || index > 4)))
+			{
+				if(row.repeat != "Customize")
+					index = 0;
+				var start = new Date();
+				console.log("here");
+				var end = new Date();
+				var sstr = row.start[index].split(":");
+				start.setHours(sstr[0]);
+				start.setMinutes(sstr[1]);
+				var estr = row.end[index].split(":");
+				end.setHours(estr[0]);
+				end.setMinutes(estr[1]);
+				if(start < d && d < end){
+					block();
+				}
+			}
+		}
+	}
+});
--- options.js
+++ options.js
@@ -1,14 +1,24 @@
+//TODO -- Validate Form
+
 //DOM Manipulations
 var createRow,updateRows;
-createRow = function(){
+createRow = function(prev){
+	if (prev === undefined){
+		prev = {patternz: "",
+			repeat: "Weekdays",
+			start: "",
+			end: ""
+		};
+	}
+	console.log(prev);
 	var tr =  $("<tr></tr>");
-	tr.append($("<td></td>").append($("<input type='text'></input>").change(updateRows)));
+	tr.append($("<td></td>").append($("<input type='text'></input>").change(updateRows).val(prev.patternz)));
 	tr.append($("<td></td>").append($("<select>"
 		+ "<option value='Weekdays'>Weekdays</option>"
 		+ "<option value='Weekends'>Weekends</option>"
 		+ "<option value='Everyday'>Everyday</option>"
 		+ "<option value='Customize'>Customize</option>"
-		+ "</select>").change(repeatSelectScript)));
+		+ "</select>").change(repeatSelectScript).val(prev.repeat)));
 	tr.append($("<td></td>").append($("<select>"
 		+ "<option value='0'>Sunday</option>"
 		+ "<option value='1'>Monday</option>"
@@ -18,28 +28,40 @@ createRow = function(){
 		+ "<option value='5'>Friday</option>"
 		+ "<option value='6'>Saturday</option>"
 		+ "</select>").attr("disabled",true).change(daySelect)));
-	var  inputs = $("<td></td>");
+	var  stime = $("<td></td>");
+	var  etime = $("<td></td>");
 	for(var i=0; i < 7; i++){
-		if(i==0)
-			inputs.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","inline"));
+		if(i==0){
+			stime.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","inline").val(prev.start[i]));
+			etime.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","inline").val(prev.end[i]));
+		}
 		else
-			inputs.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","none"));
+		{
+			stime.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","none").val(prev.start[i]));
+			etime.append($("<input type='time' class='time"+i.toString()+"'></input>").css("display","none").val(prev.end[i]));
+		}
 	}
-	tr.append(inputs);
+	tr.append(stime,etime);
 	return tr;
 };
 var repeatSelectScript = function(){
 	if($(this).val()==="Customize"){
 		$(this).parent().next().children().attr("disabled", false);
-		$(this).parent().next().next().children().attr("disabled", false);
+		//$(this).parent().next().next().children().attr("disabled", false);
+		//$(this).parent().next().next().next().children().attr("disabled", false);
 	}
-	else
+	else{
 		$(this).parent().next().children().attr("disabled", true);
-	$(this).parent().next().next().children().attr("disabled", true);
+		$(this).parent().next().children().val("0").trigger("change");
+		//$(this).parent().next().next().children().attr("disabled", true);
+		//$(this).parent().next().next().next().children().attr("disabled", true);
+	}
 };
 var daySelect = function(){
 	$(this).parent().next().children().css("display","none");
+	$(this).parent().next().next().children().css("display","none");
 	$(this).parent().next().children().eq($(this).val()).css("display","inline");
+	$(this).parent().next().next().children().eq($(this).val()).css("display","inline");
 }
 updateRows = function(){
 	var enputs = 0; //Counts empty inputs
@@ -54,6 +76,45 @@ updateRows = function(){
 	if(enputs==0)
 		$("#setting_table").append(createRow());
 };
-updateRows();
 
 //End DOM Manip
+
+
+var saveOptions = function(){
+	var data = [];
+	var record;
+	$("#setting_table").children().each(function(){
+		record = {};
+		record.patternz = $(this).children().eq(0).children().first().val();
+		if(record.patternz != ""){
+			record.repeat = $(this).children().eq(1).children().eq(0).val();
+			record.start = [];
+			record.end = [];
+			for(var i = 0; i < 7; i++){
+				record.start.push($(this).children().eq(3).children().eq(i).val());
+				record.end.push($(this).children().eq(4).children().eq(i).val());
+			}
+			data.push(record);
+		}
+	})
+	console.log(data);
+	chrome.storage.sync.set({'options': data},function(){
+		console.log('Settings Saved');
+	});
+}
+
+$("input[type='submit'").click(saveOptions);
+
+var initialize = function(){
+	chrome.storage.sync.get('options',function(result){
+		nrows = result.options.length;
+		for(var i = 0; i < nrows; i++){
+			$("#setting_table").append(createRow(result.options[i]));
+		}
+		$("#setting_table").append(createRow());
+		$("select").trigger("change");
+	});
+	//console.log(data);
+	
+};
+initialize();
